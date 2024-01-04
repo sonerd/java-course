43cf8cff6bbb3ffda0cfd774f9ebdb9d539060b6
Soner Dastan <son.dastan@gmail.com>
2024-01-04 12:09:43 +0100

    using BufferedReader

--- src/week4/files/FileMain.java
+++ src/week4/files/FileMain.java
@@ -1,8 +1,14 @@
 package week4.files;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileMain {
 
@@ -14,6 +20,7 @@ public class FileMain {
         File fileWithRelativePath = new File("test.txt");
         checkFileExistence(fileWithRelativePath);
 
+        // working with directories
         File directory = new File("C:\\Users\\USER\\OneDrive\\Documents\\GitHub\\java-course\\src\\week2");
         checkFileExistence(directory);
         if (directory.isDirectory()) {
@@ -25,6 +32,62 @@ public class FileMain {
                }
             }
             System.out.println(fileToPrint);
+        }
+
+
+        // creating a file
+        File tmpFile = new File("tmp.txt");
+        if (!tmpFile.exists()) {
+            try {
+                final boolean createdFile = tmpFile.createNewFile();
+                if (createdFile) {
+                    System.out.println("File created: " + tmpFile.getName());
+                }
+            } catch (IOException e) {
+                System.out.println("Could not create temporary file, cause: " + e.getMessage());
+            }
+        } else {
+            System.out.println("File " + tmpFile.getName() + " already exist!");
+        }
+
+        // reading from a file with FileInputStream
+        final File txtFile = new File("text.txt");
+        try {
+            FileInputStream fis = new FileInputStream(txtFile);
+            while(fis.read() != -1) {
+                //System.out.println("File input: " + fis.read());
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        } catch (IOException e) {
+            System.out.println("Cannot read file, cause: " + e.getMessage());
+        }
+
+
+        // reading a text file using BufferedReader
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(txtFile));
+            String line;
+            // using looping
+            System.out.println("----Line by Line----");
+            while ((line = reader.readLine()) != null) {
+                System.out.println("Line of file: " + line);
+            }
+
+            // read all lines and store in a list
+            System.out.println("----All lines once----");
+            reader.reset();
+            List<String> allLines = reader.lines().collect(Collectors.toList());
+            for (String lineOfFile : allLines) {
+                System.out.println("Line of file: " + lineOfFile);
+            }
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found, cause: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Cannot read file, cause: " + e.getMessage());
         }
 
     }
