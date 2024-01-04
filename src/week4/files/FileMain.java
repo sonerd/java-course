e092c3162f6da6a700d1b8d6df1248c341298da4
Farisalali789 <152271757+Farisalali789@users.noreply.github.com>
2024-01-04 11:08:43 +0100

    Made the code print an array of files

--- src/week4/files/FileMain.java
+++ src/week4/files/FileMain.java
@@ -1,24 +1,30 @@
 package week4.files;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileMain {
 
     public static void main(String[] args) {
-        final String absolutePath = "/Users/soner/Downloads/Lecture7/BCS15_Fall2023_LectureNotes_AWT_vs_Swing.ppt";
+        final String absolutePath = "C:\\Users\\USER\\OneDrive\\Documents\\GitHub\\java-course\\src\\week2";
         File fileWithAbsolutePath = new File(absolutePath);
         checkFileExistence(fileWithAbsolutePath);
 
         File fileWithRelativePath = new File("test.txt");
         checkFileExistence(fileWithRelativePath);
 
-        File directory = new File("/Users/soner/Downloads/Lecture7");
+        File directory = new File("C:\\Users\\USER\\OneDrive\\Documents\\GitHub\\java-course\\src\\week2");
         checkFileExistence(directory);
         if (directory.isDirectory()) {
+            List<String> fileToPrint = new ArrayList<>();
             File[] files = directory.listFiles();
-            for (File file : files) {
-                System.out.println("File in directory: " + file.getName());
+            if(files != null){
+                for (File file : files) {
+                    fileToPrint.add(file.getName());
+               }
             }
+            System.out.println(fileToPrint);
         }
 
     }
