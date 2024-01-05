08fef717396e5cbd8e89022e00bc50c4cfb09d26
Soner Dastan <son.dastan@gmail.com>
2024-01-05 11:42:37 +0100

    task for files

---
+++ src/week4/files/FileHandling.java
@@ -0,0 +1,14 @@
+package week4.files;
+
+public class FileHandling {
+
+    public static void main(String[] args) {
+
+        // 1. You have a file humans.txt containing human data as CSV format
+        // 2. Read the humans from this file
+        // 3. Create a CSV file for each nationality and write into it only the humans for that nationality
+        // 4. e.g. chinese.txt -> Chen;Long;chinese
+        //         spanish.txt -> Antonio;Silva;spanish
+        //         .......
+    }
+}
