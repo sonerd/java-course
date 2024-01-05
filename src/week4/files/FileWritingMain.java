bbf20f06471d04f54c6f9b6b12d59eaae7a50522
Farisalali789 <152271757+Farisalali789@users.noreply.github.com>
2024-01-05 11:29:45 +0100

    Used Bufferedwriter on human objects

--- src/week4/files/FileWritingMain.java
+++ src/week4/files/FileWritingMain.java
@@ -1,11 +1,15 @@
 package week4.files;
 
+import entities.Human;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileWritingMain {
 
@@ -41,5 +45,31 @@ public class FileWritingMain {
 
         // 1. Create a list of Human objects
         // 2. write all humans of the list to a CSV file using ; as separator
+
+        Human humanOne = new Human("Jake","Paul","British");
+        Human humanTwo = new Human("Eric","Kevin","Spanish");
+        Human humanThree = new Human("Shanar","AlDousiri","Saudi");
+
+        List<Human> listOfHumans = new ArrayList<>();
+        listOfHumans.add(humanOne);
+        listOfHumans.add(humanTwo);
+        listOfHumans.add(humanThree);
+        try{
+            File humansFile = new File("Humans.txt");
+            if(!humansFile.exists()){
+                humansFile.createNewFile();
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(humansFile,true));
+            for(Human human : listOfHumans){
+                String line = human.getFirstName() + ";" + human.getLastName() + ";" + human.getNationality();
+                writer.write(line);
+                writer.newLine();
+
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e){
+            System.out.println("Could not write because: "+ e.getMessage());
+        }
     }
 }
