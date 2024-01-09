ae7c930127054b24f01027c9e6e91cd83d3fda05
Soner Dastan <son.dastan@gmail.com>
2024-01-09 13:14:21 +0100

    added file for humans

--- src/week4/files/FileHandling.java
+++ src/week4/files/FileHandling.java
@@ -4,7 +4,7 @@ public class FileHandling {
 
     public static void main(String[] args) {
 
-        // 1. You have a file humans.txt containing human data as CSV format
+        // 1. You have a file humans-input.txt containing human data as CSV format
         // 2. Read the humans from this file
         // 3. Create a CSV file for each nationality and write into it only the humans for that nationality
         // 4. e.g. chinese.txt -> Chen;Long;chinese
