a26188f48118ca315c4e7aff8d7a6f22ce07bba6
Soner Dastan <son.dastan@gmail.com>
2024-01-12 08:10:33 +0100

    added more exercises

---
+++ src/week5/Exercises.java
@@ -0,0 +1,35 @@
+package week5;
+
+public class Exercises {
+
+    public static void main(String[] args) {
+
+        firstExercise();
+
+        secondExercise();
+
+        thirdExercise();
+    }
+
+    private static void firstExercise() {
+        // 1. Create a class representing a Book with attributes like title, author, and price.
+        // 2. Create a list of Book objects and sort them based on title ascending
+        // 3. Print out the sorted list
+    }
+
+    private static void secondExercise() {
+        // 1. Create a list of Book objects.
+        // 2. The list should also contain multiple Books from same author
+        // 3. How can we group the books by author?
+        // 4. which collection should be used here?
+        // 5. create the collection to group by author
+        // 6. after that create a method which we can use to get the books by a specific author
+        // 7. so the method should expect the author and return us a list of books written by the given author
+    }
+
+    private static void thirdExercise() {
+        // 1. Given the file numbers.txt whoch includes numbers in each line
+        // 2. Count the odd and even numbers and print these counts out to console
+    }
+}
+
